bc4f22e6e97c1c01bffae5cd1797db4aa03d570c
NguyenQuangMinh <ngquangminh05042001@gmail.com>
2021-08-18 13:52:44 +0700

    using tk label instead of ttk label

--- View/Widget/Result.py
+++ View/Widget/Result.py
@@ -1,13 +1,12 @@
 import tkinter as tk
-from tkinter import ttk
 
 
 class Result(tk.Toplevel):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         self.number_of_wrong_answer = tk.StringVar(value='Wrong Answers: ')
-        ttk.Label(self, textvariable=self.number_of_wrong_answer, font=('TkDefaultFont', 20)).pack()
-        frame = ttk.Frame(self)
+        tk.Label(self, textvariable=self.number_of_wrong_answer, font=('TkDefaultFont', 20)).pack()
+        frame = tk.Frame(self)
         frame.pack()
         self.wrong_word = tk.Text(frame, bd=20, width=50, height=20, font=('TkDefaultFont', 20))
         sb = tk.Scrollbar(frame)
