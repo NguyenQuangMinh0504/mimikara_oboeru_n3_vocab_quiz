cf427acf3d781933a2a9b34dc7864980db9411b4
NguyenQuangMinh <ngquangminh05042001@gmail.com>
2021-08-21 15:54:15 +0700

    small code change

--- View/GUI.py
+++ View/GUI.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-from tkinter import PhotoImage
-
 import Setting.Load
 from View.Widget.Frame.FirstFrame import FirstFrame
 from View.Widget.Menu import MainMenu
@@ -20,7 +18,7 @@ class Application(tk.Tk):
         self.frame.pack()
 
         # setting the logo
-        logo = PhotoImage(file=Setting.Load.logo_path)
+        logo = tk.PhotoImage(file=Setting.Load.logo_path)
         self.iconphoto(True, logo)
 
 
