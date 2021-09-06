3dd5635b35f693c452f6e5295971f49daa930893
NguyenQuangMinh <ngquangminh05042001@gmail.com>
2021-09-06 09:57:10 +0700

    code change

--- View/Model.py
+++ View/Model.py
@@ -14,7 +14,7 @@ class Model:
     def load(self):
         self.choice_list = [i for i in range(len(self.data))]
         self.index = self.random_choice()
-        self.word = self.data.loc[self.index][0]
+        self.word = self.data.loc[self.index][1]
         self.word_count = len(self.data)
         self.frame.word.set(self.word)
         self.frame.word_count.set("Word remaining: " + str(self.word_count))
@@ -30,9 +30,9 @@ class Model:
         if status:
             self.choice_list.remove(self.index)
             self.frame.label_input_frame.spelling_input.set('')
-            self.frame.meaning.set('meaning: ' + self.data.loc[self.index][1])
-            self.frame.spelling.set('spelling: ' + self.data.loc[self.index][2])
-            self.frame.kanji.set('kanji: ' + self.data.loc[self.index][3])
+            self.frame.meaning.set('meaning: ' + self.data.loc[self.index][2])
+            self.frame.spelling.set('spelling: ' + self.data.loc[self.index][3])
+            self.frame.kanji.set('kanji: ' + self.data.loc[self.index][4])
             self.frame.status.config(foreground='green', text='Correct')
             if self.frame.parent.menu_bar.sound.get():
                 Sound.play_right_sound()
@@ -41,22 +41,22 @@ class Model:
                 self.display_result()
             else:
                 self.index = self.random_choice()
-                self.frame.word.set(self.data.loc[self.index][0])
+                self.frame.word.set(self.data.loc[self.index][1])
 
         else:
             if self.index not in self.wrong_ans:
                 self.wrong_ans.append(self.index)
             self.frame.status.config(foreground='red', text='Incorrect')
-            self.frame.meaning.set('meaning: ' + self.data.loc[self.index][1])
-            self.frame.spelling.set('spelling: ' + self.data.loc[self.index][2])
-            self.frame.kanji.set('kanji: ' + self.data.loc[self.index][3])
+            self.frame.meaning.set('meaning: ' + self.data.loc[self.index][2])
+            self.frame.spelling.set('spelling: ' + self.data.loc[self.index][3])
+            self.frame.kanji.set('kanji: ' + self.data.loc[self.index][4])
 
             if self.frame.parent.menu_bar.sound.get():
                 Sound.play_wrong_sound()
         self.frame.word_count.set('Word remaining: ' + str(len(self.choice_list)))
 
     def handle(self, event=None):
-        if self.frame.label_input_frame.spelling_input.get() == self.data.loc[self.index][2]:
+        if self.frame.label_input_frame.spelling_input.get() == self.data.loc[self.index][3]:
             self.print_result(True)
         else:
             self.print_result(False)
@@ -71,23 +71,11 @@ class Model:
         result = Result(self.frame.parent)
         result.number_of_wrong_answer.set(result.number_of_wrong_answer.get() +
                                           '{}/{}'.format(len(self.wrong_ans), len(self.data)))
-        # displaying index
-        unit = int(self.frame.unit.replace("Unit ", ""))
-        k = 0
-        if unit == 1:
-            k = 1
-        elif unit == 2:
-            k = 72
-        elif unit == 3:
-            k = 121
-        elif unit == 4:
-            k = 171
-        elif unit == 5:
-            k = 221
+
         self.wrong_ans.sort()
         for i in self.wrong_ans:
-            wrong_result = ', '.join([str(i+k), self.data.iloc[i][0], self.data.iloc[i][1],
-                                      self.data.iloc[i][2], self.data.iloc[i][3]])
+            wrong_result = ', '.join([str(self.data.iloc[i][0]), self.data.iloc[i][1], self.data.iloc[i][2],
+                                      self.data.iloc[i][3], self.data.iloc[i][4]])
             result.wrong_word.insert('end', wrong_result+'\n'+'------------------------'+'\n')
 
     def play_sound(self):
