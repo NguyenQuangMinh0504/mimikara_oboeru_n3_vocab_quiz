f81db7f62c90a6f854d15a288b5287ab7fb14c0c
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-08 17:14:26 +0700

    Add: Add title to window.

--- View/utils.py
+++ View/utils.py
@@ -1,7 +1,7 @@
 def get_example(word: str) -> str:
     """Return example of word using Open AI"""
     from openai import OpenAI
-    client = OpenAI(api_key="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")
+    client = OpenAI(api_key="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")
     messages = [
         {"role": "user",
          "content": "Đặt câu tiếng Nhật với từ {}. Giải thích ý nghĩa câu và các hán tự dùng trong câu.".format(word),
