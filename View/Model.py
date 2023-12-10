d48b014e38686212ddaccbc64f87ac910568f53a
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-10 21:12:00 +0700

    Mod: Refactor code

--- View/Model.py
+++ View/Model.py
@@ -6,22 +6,16 @@ class Model:
     def __init__(self, unit: str):
         self.wrong_ans = []
         self.data = Load.load_data(unit.replace(" ", "").lower())
-        self.load()
-
-    def load(self):
-        """Loading data"""
         self.choice_list = [i for i in range(len(self.data))]
         self.index = self.random_choice()
-        self.word = self.data.loc[self.index][1]
-        self.word_count = len(self.data)
 
     def get_data(self) -> dict:
-        return {"word": self.data.loc[self.index][1],
+        return {"word": self.data.iloc[self.index][1],
                 "word_count": len(self.choice_list),
-                "spelling": self.data.loc[self.index][3],
-                "word_index": str(self.data.loc[self.index][0]),
-                "meaning": self.data.loc[self.index][2],
-                "kanji": self.data.loc[self.index][4],
+                "spelling": self.data.iloc[self.index][3],
+                "word_index": str(self.data.iloc[self.index][0]),
+                "meaning": self.data.iloc[self.index][2],
+                "kanji": self.data.iloc[self.index][4],
                 }
 
     def random_choice(self):
