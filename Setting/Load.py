c15ddf1f3a0da059f22d27781d2d9c9594307227
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-13 22:18:02 +0700

    Mod: Fix path problems when build with pyinstaller

--- Setting/Load.py
+++ Setting/Load.py
@@ -1,20 +1,40 @@
 import json
 import pandas as pd
 import sys
+import os
 
-right_sound_path = './Assets/Sound/right_sound.wav'
-wrong_sound_path = './Assets/Sound/wrong_sound.wav'
-logo_path = "./Assets/Image/app_icon-2.gif"
-sound_button_path = "./Assets/Image/50px_sound_button.gif"
+unit_complete_path = "Data/Status/unit_complete.json"
+user_status_path = "Data/Status/user_status.txt"
+sound_setting_path = "Data/Status/sound_setting.json"
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+
+    # Problems
+    # https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+right_sound_path = resource_path("Assets/Sound/right_sound.wav")
+wrong_sound_path = resource_path("Assets/Sound/wrong_sound.wav")
+logo_path = resource_path("Assets/Image/app_icon-2.gif")
+sound_button_path = resource_path("Assets/Image/50px_sound_button.gif")
 
 
 def get_unit_complete():
-    with open("./Data/Status/unit_complete.json") as json_file:
+    with open(resource_path(unit_complete_path)) as json_file:
         return json.load(json_file)
 
 
 def set_unit_complete(data):
-    with open("./Data/Status/unit_complete.json", 'w') as json_file:
+    with open(resource_path(unit_complete_path), 'w') as json_file:
         json.dump(data, json_file)
 
 
@@ -22,28 +42,29 @@ def load_data(file) -> pd.DataFrame:
     """
     Loading unit vocabulary from unit.csv file"""
     try:
-        path = './Data/Dictionary/'
-        return pd.read_csv(path + file + '.csv')
+        path = resource_path("Data/Dictionary/") + file + ".csv"
+        return pd.read_csv(path)
     except FileNotFoundError:
+        print(path)
         print(sys.path)
         print("File not found")
 
 
 def add_active_day(day):
-    with open('./Data/Status/user_status.txt', 'a') as f:
+    with open(resource_path(user_status_path), 'a') as f:
         f.write('\n' + day)
 
 
 def get_active_day():
-    with open('./Data/Status/user_status.txt', 'r') as f:
+    with open(resource_path(user_status_path), 'r') as f:
         return [i.rstrip() for i in f.readlines()]
 
 
 def get_sound_setting():
-    with open("./Data/Status/sound_setting.json", 'r') as json_file:
+    with open(resource_path(sound_setting_path), 'r') as json_file:
         return json.load(json_file)
 
 
 def set_sound_setting(data):
-    with open("./Data/Status/sound_setting.json", 'w') as json_file:
+    with open(resource_path(sound_setting_path), 'w') as json_file:
         json.dump(data, json_file)
